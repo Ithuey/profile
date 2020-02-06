496a6655e47abd7f132aa80060fced940b372de1
carpenth <carpenth@mail.uc.edu>
2020-02-06 01:46:24 -0500

    Added automatic email for contact info

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import { motion } from "framer-motion";
 import tree from "./assets/tree.svg";
+
+import emailjs from "emailjs-com";
 
 import { FaLinkedin, FaGithub } from "react-icons/fa";
 import { IoIosMail, IoIosContact } from "react-icons/io";
@@ -8,6 +10,36 @@ import { IoIosMail, IoIosContact } from "react-icons/io";
 import "../Contact.css";
 
 function Contact() {
+  // const [name, setName] = useState("hey");
+  // const [email, setEmail] = useState("");
+  // const [company, setCompany] = useState("");
+  // const [phoneNumber, setPhoneNumber] = useState("");
+  // const [position, setPosition] = useState("");
+  // const [message, setMessage] = useState("");
+
+  function sendEmail(e) {
+    e.preventDefault(e);
+
+    emailjs
+      .sendForm(
+        "gmail",
+        "template_lcRiGG27",
+        e.target,
+        "user_B8aFDogO8YzahD86OMP9u"
+      )
+      .then(
+        results => {
+          console.log("Success!", results.text);
+          alert("Thanks for your interest!");
+        },
+        error => {
+          console.log("hmm, that's weird.", error.text);
+          alert("Hmm, didnt work.");
+        }
+      );
+    e.target.reset();
+  }
+
   return (
     <div className="container">
       <div className="animationContainerContact">
@@ -53,37 +85,86 @@ function Contact() {
           transition={{ duration: 1.6 }}
         />
 
-        {/* <motion.div
-                style={{
-                    zIndex: 3,
-                    position: 'absolute',
-                }}
-
-                initial={{
-                    x: 15,
-                    y: -200
-                }}
-
-                animate={{
-                    x: 15,
-                    y: 350
-                }}
-                transition={{ duration: 1.9, type: 'spring', damping: 100 }}
-            >
-                <img src={tree} className='tree' />
-            </motion.div> */}
+        <motion.div
+          style={{
+            zIndex: 5,
+            position: "absolute"
+          }}
+          initial={{
+            x: 15,
+            y: -200
+          }}
+          animate={{
+            x: 15,
+            y: 350
+          }}
+          transition={{ duration: 1.9, type: "spring", damping: 100 }}
+        >
+          <img src={tree} className="tree" />
+        </motion.div>
       </div>
       <div className="textContainer">
         <ul className="contactUlText">
           <li>
             <IoIosContact size={60} />
           </li>
-          <li style={{ fontSize: "2em" }}>
-            <IoIosMail size={20} /> Thueycarp@gmail.com
-          </li>
           <li>
-            <FaLinkedin size={30} /> <FaGithub size={30} />
+            <h2>LET'S TALK</h2>
           </li>
+          <form style={{ color: "red" }} onSubmit={sendEmail}>
+            <li>
+              <input
+                type="text"
+                placeholder="Name"
+                name="contact_name"
+                required
+              />
+            </li>
+
+            <li>
+              <input
+                type="email"
+                placeholder="Email"
+                name="contact_email"
+                required
+              />
+            </li>
+            <li>
+              <input
+                type="tel"
+                placeholder="Phone Number"
+                name="contact_phoneNumber"
+                required
+              />
+            </li>
+            <li>
+              <input
+                type="text"
+                placeholder="Company"
+                name="contact_company"
+                required
+              />
+            </li>
+            <li>
+              <input
+                type="text"
+                placeholder="Position"
+                name="contact_position"
+                required
+              />
+            </li>
+            <li>
+              <textarea
+                placeholder="Message"
+                rows="5"
+                name="contact_message"
+                required
+              />
+            </li>
+            <button type="Submit" value="Send" className="btnSubmit">
+              Send
+            </button>
+          </form>
         </ul>
       </div>
     </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -3,6 +3,8 @@ import { motion } from "framer-motion";
 import "../Header.css";
 import { Link } from "react-router-dom";
 
+import { FaLinkedin, FaGithub } from "react-icons/fa";
+
 function Header() {
   return (
     <div>
@@ -25,6 +27,24 @@ function Header() {
             </motion.li>
             <motion.li whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }}>
               <Link to="/contact">Contact</Link>
+            </motion.li>
+          </ul>
+        </nav>
+        <nav className="navIcons">
+          <ul>
+            <motion.li
+              className="navIcons"
+              whileHover={{ scale: 1.1 }}
+              whileTap={{ scale: 0.9 }}
+            >
+              <FaLinkedin size={30} />
+            </motion.li>
+            <motion.li
+              className="navIcons"
+              whileHover={{ scale: 1.1 }}
+              whileTap={{ scale: 0.9 }}
+            >
+              <FaGithub size={30} />
             </motion.li>
           </ul>
         </nav>

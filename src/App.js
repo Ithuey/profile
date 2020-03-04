96b0dbcc1ff1ef07c530ae7ec70809f6cb1d9fea
carpenth <carpenth@mail.uc.edu>
2020-03-04 10:23:31 -0500

    changed the home screen

--- src/App.js
+++ src/App.js
@@ -1,28 +1,23 @@
-import React from 'react';
+import React from "react";
 import { BrowserRouter, Route } from "react-router-dom";
 
-import Header from './components/Header';
-import Home from './components/Home';
-import Project from './components/Project';
-import Contact from './components/Contact'
-
-
+import Header from "./components/Header";
+import Home from "./components/Home";
+import Project from "./components/Project";
+import Contact from "./components/Contact";
 
 //import './App.css';
 
 function App() {
-
-
   return (
     <BrowserRouter>
-      <Header />
       <React.Fragment>
         <Route exact path="/" component={Home} />
+        <Header />
         <Route path="/projects" component={Project} />
         <Route path="/contact" component={Contact} />
       </React.Fragment>
     </BrowserRouter>
-
   );
 }
 
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,14 @@
 import React, { useState } from "react";
-import Header from "./Header";
 import { motion } from "framer-motion";
-import circle from "./assets/circle.svg";
-import tree from "./assets/tree.svg";
+
 import arrow from "../arrow.svg";
 import { Link, Redirect } from "react-router-dom";
+import video from "./assets/redmaple.mp4";
+
+import PDFResume from "../documents/ToddCarpenterResume.pdf";
+
+import { FaLinkedin, FaGithub } from "react-icons/fa";
+import { TiDocument } from "react-icons/ti";
 
 import "../Home.css";
 import "../App.css";
@@ -14,67 +18,8 @@ function Home() {
   return (
     <div className="container">
       <div className="animationContainerHome">
-        <motion.div
-          style={{
-            width: 1000,
-            height: 1000,
-            borderRadius: 600,
-            background: "#44af69",
-            position: "absolute",
-            zIndex: 2,
-            boxShadow: "0px 3px 10px rgba(0, 0,0,1)"
-          }}
-          initial={{
-            x: -400,
-            y: 920
-          }}
-          animate={{
-            x: -400,
-            y: 580
-          }}
-          transition={{ duration: 1.6 }}
-        />
-        <motion.div
-          style={{
-            width: 1000,
-            height: 1000,
-            borderRadius: 600,
-            background: "#095256",
-            overflow: "hidden",
-            position: "absolute",
-
-            zIndex: 1,
-            boxShadow: "0px 3px 10px rgba(0, 0,0,0.5)"
-          }}
-          initial={{
-            x: 1,
-            y: 920
-          }}
-          animate={{
-            x: 1,
-            y: 580
-          }}
-          transition={{ duration: 1.6 }}
-        />
+        <video autoPlay muted loop src={video}></video>
       </div>
-
-      {/* <motion.div
-        style={{
-          zIndex: 3,
-          position: "absolute"
-        }}
-        initial={{
-          x: 15,
-          y: -200
-        }}
-        animate={{
-          x: 15,
-          y: 350
-        }}
-        transition={{ duration: 1.9, type: "spring", damping: 100 }}
-      >
-        <img src={tree} className="tree" />
-      </motion.div> */}
       <div className="textContainer">
         <ul className="ulText">
           <li style={{ fontSize: "50px" }}>
@@ -93,6 +38,42 @@ function Home() {
               </button>
             </Link>
           </li>
+        </ul>
+      </div>
+
+      <div className="iconContainer">
+        <ul className="icons">
+          <motion.li
+            className="icons"
+            whileHover={{ scale: 1.1 }}
+            whileTap={{ scale: 0.9 }}
+          >
+            <a target="_blank" href={PDFResume} title="Resume">
+              <TiDocument size={30} />
+            </a>
+          </motion.li>
+          <motion.li
+            className="icons"
+            whileHover={{ scale: 1.1 }}
+            whileTap={{ scale: 0.9 }}
+          >
+            <a
+              target="_blank"
+              href="https://linkedin.com/in/todd-houston-carpenter"
+              title="LinkedIn"
+            >
+              <FaLinkedin size={30} />
+            </a>
+          </motion.li>
+          <motion.li
+            className="icons"
+            whileHover={{ scale: 1.1 }}
+            whileTap={{ scale: 0.9 }}
+          >
+            <a target="_blank" href="https://github.com/Ithuey" title="GitHub">
+              <FaGithub size={30} />
+            </a>
+          </motion.li>
         </ul>
       </div>
     </div>
